cfcb518c7de74d57609edaa60c095ef031c173c1
daniela-ruizp <daniela.ruizp@alumnos.upm.es>
2025-10-11 11:42:05 +0200

    Update bubbleRecur.java

--- Factoriales/src/es/upm/aled/factorial/bubbleRecur.java
+++ Factoriales/src/es/upm/aled/factorial/bubbleRecur.java
@@ -2,4 +2,22 @@ package es.upm.aled.factorial;
 
 public class bubbleRecur {
 
+	public void bubbleRecur(int[] array) {
+		bubbleRecur(array,0);
+
+	}
+
+	private void bubbleRecur(int[] array, int posicion) {
+		do {
+			if (array[posicion] < array[posicion+1])
+				posicion = posicion+2;
+			else if (array[posicion] >= array[posicion+1]) {
+				int aux = array[posicion];
+				array[posicion] = array[posicion+1];
+				array[posicion+1] = aux;
+				posicion=posicion+2;
+
+			}
+		} while (posicion < array.length);
+	}
 }
